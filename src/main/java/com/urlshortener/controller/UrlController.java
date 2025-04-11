cd7d69b4948026b6b8576442299673b583a05175
AlexGirshfeld <alex.girshfeld@gmail.com>
2025-04-11 15:50:35 +0300

    d

--- src/main/java/com/urlshortener/controller/UrlController.java
+++ src/main/java/com/urlshortener/controller/UrlController.java
@@ -1,7 +1,12 @@
 package com.urlshortener.controller;
 
 import com.urlshortener.service.UrlService;
+
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,7 +22,14 @@ public class UrlController {
     }
 
     @GetMapping("/{shortUrl}")
-    public String getOriginalUrl(@PathVariable String shortUrl) {
-        return urlService.getOriginalUrl(shortUrl);
+public ResponseEntity<Void> redirectToOriginalUrl(@PathVariable String shortUrl) {
+    String originalUrl = urlService.getOriginalUrl(shortUrl);
+    if (originalUrl != null) {
+        // Redirect to the original URL
+        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(originalUrl)).build();
+    } else {
+        // Return 404 if the shortened URL is not found
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
     }
 }
